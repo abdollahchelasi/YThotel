c9f4a29a7aa378a1df5c2690efec222e802e355b
abdollah <32410133+abdollahchelasi@users.noreply.github.com>
2024-01-20 12:09:55 +0330

    Create app.py

---
+++ app.py
@@ -0,0 +1,17 @@
+# https://www.youtube.com/watch?v=AqtLEgn1RoE
+
+
+
+import streamlit as st
+from pytube import YouTube 
+
+link = st.text_input("Enter URL : ")
+
+if st.button("Download"):
+    video = YouTube(link)
+    stream = video.streams.get_highest_resolution()
+    st.text("Downloading...")
+    file_path = stream.download()
+    st.text("Download complete!")
+    # Display video
+    st.video(file_path)
