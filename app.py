370af5154cb5ae86442aecbbe91f1fba63dae2fa
abdollah <32410133+abdollahchelasi@users.noreply.github.com>
2024-01-21 11:07:09 +0330

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,78 @@
 # https://www.youtube.com/watch?v=AqtLEgn1RoE
 
-
-
 import streamlit as st
 from pytube import YouTube 
+import re
 
 link = st.text_input("Enter URL : ")
 
-if st.button("Download"):
-    videoo = YouTube(link)
-    stream = videoo.streams.get_highest_resolution()
-    st.text("Downloading...")
-    file_path = stream.download()
-    st.text("Download complete!")
-    # Display video
-    st.video(file_path)
+video_id_match = re.search(r"(?:v=|\/)([0-9A-Za-z_-]{11})", link)
+video_id = video_id_match.group(1) if video_id_match else None
+
+if video_id:
+    video = YouTube(f"https://www.youtube.com/watch?v={video_id}")
+    available_streams = video.streams.filter(file_extension="mp4").all()
+    stream_quality = st.selectbox("Select Video Quality", [str(stream.resolution) for stream in available_streams])
+
+    if st.button("Download"):
+        selected_stream = next((stream for stream in available_streams if str(stream.resolution) == stream_quality), None)
+        if selected_stream:
+            st.text("Downloading...")
+            file_path = selected_stream.download()
+            st.text("Download complete!")
+            # Display video
+            st.video(file_path)
+        else:
+            st.text("Selected video quality is not available.")
+else:
+    st.text("Invalid YouTube URL.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import streamlit as st
+# from pytube import YouTube 
+
+# link = st.text_input("Enter URL : ")
+
+# if st.button("Download"):
+#     videoo = YouTube(link)
+#     stream = videoo.streams.get_highest_resolution()
+#     st.text("Downloading...")
+#     file_path = stream.download()
+#     st.text("Download complete!")
+#     # Display video
+#     st.video(file_path)
 
 
