90b6c05bcfeb2cb3f9a9f98298d77111a117b46e
abdollah <32410133+abdollahchelasi@users.noreply.github.com>
2024-01-21 17:02:37 +0330

    Update app.py

--- app.py
+++ app.py
@@ -8,28 +8,16 @@ link = st.text_input("Enter URL : ")
 if st.button("Download"):
     try:
         video = YouTube(link)
-        available_streams = video.streams.filter(file_extension="mp4").all()
-        stream_quality = st.selectbox("Select Video Quality", [str(stream.resolution) for stream in available_streams])
-
-        if stream_quality:
-            selected_stream = next((stream for stream in available_streams if str(stream.resolution) == stream_quality), None)
-            if selected_stream:
-                st.text("Downloading...")
-                file_path = selected_stream.download(filename=f"{video.title}.mp4", path=st.get_downloads_path())
-                st.text("Download complete!")
-
-                # Provide a download link and display the video with audio
-                st.markdown(f"**Downloaded video:** [Link](file://{file_path})")
-
-                # Display the selected video quality
-                st.video(file_path, width=selected_stream.width)
-            else:
-                st.text("Selected video quality is not available.")
-        else:
-            st.text("No suitable video stream found.")
+        stream = video.streams.get_highest_resolution()
+        st.text("Downloading...")
+        file_path = stream.download()
+        st.text("Download complete!")
+        # Display video
+        st.video(file_path)
     except:
         st.error("Please enter a valid YouTube URL.")
 
+   
 
 
 
