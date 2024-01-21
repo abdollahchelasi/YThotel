4f03743829976e2c9971fd07629143dcf07c4b2c
abdollah <32410133+abdollahchelasi@users.noreply.github.com>
2024-01-21 16:58:27 +0330

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,22 @@
 # https://www.youtube.com/watch?v=AqtLEgn1RoE
 
 import streamlit as st
-from pytube import YouTube 
+from pytube import YouTube
 
 link = st.text_input("Enter URL : ")
 
 if st.button("Download"):
-    video = YouTube(link)
-    stream = video.streams.get_highest_resolution()
-    st.text("Downloading...")
-    file_path = stream.download()
-    st.text("Download complete!")
-    # Display video
-    st.video(file_path)
+    try:
+        video = YouTube(link)
+        stream = video.streams.get_highest_resolution()
+        st.text("Downloading...")
+        file_path = stream.download()
+        st.text("Download complete!")
+        # Display video
+        st.video(file_path)
+    except:
+        st.error("Please enter a valid YouTube URL.")
+
 
 
 
