115ae8958675a5df0498cf679844f64d43b03eeb
abdollah <32410133+abdollahchelasi@users.noreply.github.com>
2024-01-22 17:56:25 +0330

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 # https://www.youtube.com/watch?v=AqtLEgn1RoE
 
-
 import streamlit as st
 from pytube import YouTube 
 import re
@@ -11,25 +10,22 @@ video_id_match = re.search(r"(?:v=|\/)([0-9A-Za-z_-]{11})", link)
 video_id = video_id_match.group(1) if video_id_match else None
 
 if video_id:
-    video = YouTube(f"https://www.youtube.com/watch?v={video_id}")
-    available_streams = video.streams.filter(file_extension="mp4").all()
-    stream_quality = st.selectbox("Select Video Quality", [str(stream.resolution) for stream in available_streams])
+   video = YouTube(f"https://www.youtube.com/watch?v={video_id}")
+   available_streams = video.streams.filter(file_extension="mp4").all()
+   stream_quality = st.selectbox("Select Video Quality", [str(stream.resolution) for stream in available_streams])
 
-    if st.button("Download"):
-        selected_stream = next((stream for stream in available_streams if str(stream.resolution) == stream_quality), None)
-        if selected_stream:
-            st.text("Downloading...")
-            file_path = selected_stream.download(filename=f"{video.title}.mp4", path=st.get_downloads_path())  # Download to user's downloads path
-            st.text("Download complete!")
-
-            # Provide a download link for the downloaded video
-            st.markdown(f"**Downloaded video:** [Link](file://{file_path})")
-        else:
-            st.text("Selected video quality is not available.")
+   if st.button("Download"):
+       selected_stream = next((stream for stream in available_streams if str(stream.resolution) == stream_quality), None)
+       if selected_stream:
+           st.text("Downloading...")
+           file_path = selected_stream.download()
+           st.text("Download complete!")
+           # Display video
+           st.video(file_path)
+       else:
+           st.text("Selected video quality is not available.")
 else:
-    st.text("Invalid YouTube URL.")
-
-
+   st.text("Invalid YouTube URL.")
 
 
 
