1db08f827b92a1790f81d15dc78106de76ccbf2e
abdollah <32410133+abdollahchelasi@users.noreply.github.com>
2024-01-22 18:46:58 +0330

    Update app.py

--- app.py
+++ app.py
@@ -1,29 +1,37 @@
 # https://www.youtube.com/watch?v=AqtLEgn1RoE
 
 
+
 import streamlit as st
 from pytube import YouTube 
 import re
 
 link = st.text_input("Enter URL : ")
 
-if st.button("Show Qualities"):
-  video_id_match = re.search(r"(?:v=|\/)([0-9A-Za-z_-]{11})", link)
-  video_id = video_id_match.group(1) if video_id_match else None
 
-  if video_id:
-    video = YouTube(f"https://www.youtube.com/watch?v={video_id}")
-    available_streams = video.streams.filter(file_extension="mp4").all()
-    
-    st.text("Available video qualities:")
-    for stream in available_streams:
-      st.write(f"* {stream.resolution}")
-  else:
-    st.error("Invalid YouTube URL.")
+if st.button("Ok"):
+    video_id_match = re.search(r"(?:v=|\/)([0-9A-Za-z_-]{11})", link)
+    video_id = video_id_match.group(1) if video_id_match else None
 
-if st.button("Download"):
-  # Implementez your existing download logic here using video_id and selected_stream
-  # ...
+
+
+if video_id:
+   video = YouTube(f"https://www.youtube.com/watch?v={video_id}")
+   available_streams = video.streams.filter(file_extension="mp4").all()
+   stream_quality = st.selectbox("Select Video Quality", [str(stream.resolution) for stream in available_streams])
+
+   if st.button("Download"):
+       selected_stream = next((stream for stream in available_streams if str(stream.resolution) == stream_quality), None)
+       if selected_stream:
+           st.text("Downloading...")
+           file_path = selected_stream.download()
+           st.text("Download complete!")
+           # Display video
+           st.video(file_path)
+       else:
+           st.text("Selected video quality is not available.")
+else:
+   st.error("Invalid YouTube URL.")
 
 
 
