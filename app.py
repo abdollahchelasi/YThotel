5dc7f4a20cfae35ed7945a48309d7fdfdd4f5931
abdollah <32410133+abdollahchelasi@users.noreply.github.com>
2024-01-22 18:48:20 +0330

    Update app.py

--- app.py
+++ app.py
@@ -7,48 +7,28 @@ import re
 
 link = st.text_input("Enter URL : ")
 
-if st.button("Show Qualities"):
-  video_id_match = re.search(r"(?:v=|\/)([0-9A-Za-z_-]{11})", link)
-  video_id = video_id_match.group(1) if video_id_match else None
+video_id_match = re.search(r"(?:v=|\/)([0-9A-Za-z_-]{11})", link)
+video_id = video_id_match.group(1) if video_id_match else None
 
-  if video_id:
-    video = YouTube(f"https://www.youtube.com/watch?v={video_id}")
-    available_streams = video.streams.filter(file_extension="mp4").all()
-    
-    st.text("Available video qualities:")
-    for stream in available_streams:
-      st.write(f"* {stream.resolution}")
-  else:
-    st.error("Invalid YouTube URL.")
 
-if st.button("Download"):
-  video_id_match = re.search(r"(?:v=|\/)([0-9A-Za-z_-]{11})", link)
-  video_id = video_id_match.group(1) if video_id_match else None
 
-  if video_id:
-    video = YouTube(f"https://www.youtube.com/watch?v={video_id}")
-    available_streams = video.streams.filter(file_extension="mp4").all()
+if video_id:
+   video = YouTube(f"https://www.youtube.com/watch?v={video_id}")
+   available_streams = video.streams.filter(file_extension="mp4").all()
+   stream_quality = st.selectbox("Select Video Quality", [str(stream.resolution) for stream in available_streams])
 
-    # Select the quality from the dropdown menu
-    stream_quality = st.selectbox("Select Video Quality", [str(stream.resolution) for stream in available_streams])
-
-    if stream_quality:
-      selected_stream = next((stream for stream in available_streams if str(stream.resolution) == stream_quality), None)
-
-      if selected_stream:
-        st.text("Downloading...")
-        file_path = selected_stream.download(filename=f"{video.title}.mp4", path=st.get_downloads_path())
-        st.text("Download complete!")
-
-        # Display download link and video
-        st.markdown(f"**Downloaded video:** [Link](file://{file_path})")
-        st.video(file_path, width=selected_stream.width)
-      else:
-        st.text("Selected video quality is not available.")
-    else:
-      st.text("No suitable video stream found.")
-  else:
-    st.error("Invalid YouTube URL.")
+   if st.button("Download"):
+       selected_stream = next((stream for stream in available_streams if str(stream.resolution) == stream_quality), None)
+       if selected_stream:
+           st.text("Downloading...")
+           file_path = selected_stream.download()
+           st.text("Download complete!")
+           # Display video
+           st.video(file_path)
+       else:
+           st.text("Selected video quality is not available.")
+else:
+   st.error("Invalid YouTube URL.")
 
 
 
