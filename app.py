71aaf164e2585bd276ed7ff8bac7f65ad519acaf
abdollah <32410133+abdollahchelasi@users.noreply.github.com>
2024-01-22 17:46:37 +0330

    Update app.py

--- app.py
+++ app.py
@@ -8,14 +8,60 @@ link = st.text_input("Enter URL : ")
 if st.button("Download"):
     try:
         video = YouTube(link)
-        stream = video.streams.get_highest_resolution()
-        st.text("Downloading...")
-        file_path = stream.download()
-        st.text("Download complete!")
-        # Display video
-        st.video(file_path)
+        available_streams = video.streams.filter(file_extension="mp4").all()
+        stream_quality = st.selectbox("Select Video Quality", [str(stream.resolution) for stream in available_streams])
+
+        if stream_quality:
+            selected_stream = next((stream for stream in available_streams if str(stream.resolution) == stream_quality), None)
+            if selected_stream:
+                st.text("Downloading...")
+                file_path = selected_stream.download(filename=f"{video.title}.mp4", path=st.get_downloads_path())
+                st.text("Download complete!")
+
+                # Display download link and video
+                st.markdown(f"**Downloaded video:** [Link](file://{file_path})")
+                st.video(file_path, width=selected_stream.width)
+            else:
+                st.text("Selected video quality is not available.")
+        else:
+            st.text("No suitable video stream found.")
     except:
         st.error("Please enter a valid YouTube URL.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import streamlit as st
+# from pytube import YouTube
+
+# link = st.text_input("Enter URL : ")
+
+# if st.button("Download"):
+#     try:
+#         video = YouTube(link)
+#         stream = video.streams.get_highest_resolution()
+#         st.text("Downloading...")
+#         file_path = stream.download()
+#         st.text("Download complete!")
+#         # Display video
+#         st.video(file_path)
+#     except:
+#         st.error("Please enter a valid YouTube URL.")
 
    
 
